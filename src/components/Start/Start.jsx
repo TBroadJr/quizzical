99ee1ba317e34f61e846fcbffc9a96007f830a88
Tornelius Broadwater Jr <tkbjr01@icloud.com>
2024-04-18 09:10:30 -0400

    Update Start.jsx

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import "./Start.css"
+import yellowBlob from "../../assets/blob-5.png"
+import blueBlob from "../../assets/blob-6.png"
+
 
 export default function Start({buttonSelect}) {
   return (
     <div className="main-div">
-      <img className="yellow-blob" src="/src/assets/blob-5.png" alt="yellow blob" />
-      <img className="blue-blob" src="/src/assets/blob-6.png" alt="blue blob" />
+      <img className="yellow-blob" src={yellowBlob} alt="yellow blob" />
+      <img className="blue-blob" src={blueBlob} alt="blue blob" />
       <div className="start-content-container">
         <h1 className="start-title">Quizzical</h1>
         <p className="start-description">Some description if needed</p>
